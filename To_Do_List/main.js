f5012a9d02ec97c64fab6f5ea091d0d371f2b629
kyunghoon <rlarudgns970@naver.com>
2022-12-12 14:21:40 +0900

    .

--- To_Do_List/main.js
+++ To_Do_List/main.js
@@ -2,6 +2,10 @@
 // + 버튼을 클릭하면, 할일이 추가된다
 // delete버튼을 누르면 할일이 삭제된다
 // check버튼을 누르면 할일이 끝나면서 밑줄이 간다
+// 1. check버튼을 클릭하는 순간 true -> false
+// 2. true이면 끝난걸로 간주하고 밑줄 보여주기
+// 3. false이면 안끝난걸로 간주하고 그대로
+
 // 진행중 탭을 누르면, 언더바가 이동한다
 // 끝남탭은 끝난 아이템만, 진행중탭은 진행중인 아이템만
 // 전체탭을 누르면 다시 전체 아이템으로 돌아옴
@@ -13,8 +17,11 @@ let taskList =[];
 addButton.addEventListener("click", addTask);
 
 function addTask(){
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+    let task = {
+        taskContent: taskInput.value,
+        isComplete: false
+    };
+    taskList.push(task);
     console.log(taskList);
     render();
 }
@@ -23,13 +30,17 @@ function render() {
     let resultHTML = "";
     for(let i=0;i<taskList.length;i++){
         resultHTML += `<div class="task">
-          <div>${taskList[i]}</div>
+          <div>${taskList[i].taskContent}</div>
           <div>
-              <button>Check</button>
+              <button onclick="toggleComplete()">Check</button>
               <button>Delete</button>
           </div>
       </div>`;
     }
 
     document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(){
+    console.log("check됐음")
 }
