9f7a402af68913bd6424808d2f5e96d53c136e82
kyunghoon <rlarudgns970@naver.com>
2022-12-13 14:21:48 +0900

    .

--- To_Do_List/main.js
+++ To_Do_List/main.js
@@ -18,6 +18,7 @@ addButton.addEventListener("click", addTask);
 
 function addTask(){
     let task = {
+        id:randomIDGenerate(),
         taskContent: taskInput.value,
         isComplete: false
     };
@@ -29,18 +30,51 @@ function addTask(){
 function render() {
     let resultHTML = "";
     for(let i=0;i<taskList.length;i++){
-        resultHTML += `<div class="task">
+        if(taskList[i].isComplete == true){
+            resultHTML+= `<div class="task">
+            <div class="task-done">${taskList[i].taskContent}</div>
+            <div>
+                <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+            </div>
+        </div>`;
+        } else{
+            resultHTML += `<div class="task">
           <div>${taskList[i].taskContent}</div>
           <div>
-              <button onclick="toggleComplete()">Check</button>
-              <button>Delete</button>
+              <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+              <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
           </div>
       </div>`;
+        }
     }
 
     document.getElementById("task-board").innerHTML = resultHTML;
 }
 
-function toggleComplete(){
-    console.log("check됐음")
+function toggleComplete(id){
+    console.log("id : ", id);
+    for(let i=0;i<taskList.length;i++){
+        if(taskList[i].id == id){
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render();
+    console.log(taskList);
+}
+
+function deleteTask(id){
+    for(let i=0; i<taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList.splice(i,1)
+            break;
+        }
+    }
+    render();
+}
+
+function randomIDGenerate(){
+    return '_' + Math.random().toString(36).substr(2, 9);
+
 }
