cca72a63419b7d2c957b9abf96fb45abcdd0d392
kyunghoon <rlarudgns970@naver.com>
2023-01-21 19:29:32 +0900

    .

--- news/main.js
+++ news/main.js
@@ -1,8 +1,6 @@
+const getLatestNews =()=>{
+    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10`);
+    console.log(url)
+}
 
-const getLatestNews = () =>{
-    let url = new URL('https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10');
-    let header = new Headers({'x-api-key' : 'bmIy8Z4kdrrGIXjYjjfTCRjDnYdhPkXvHTPcDtO-JCI'})
-    let response = fetch(url,{headers:header})
-};
-
-getLatestNews();
+getLatestNews()
