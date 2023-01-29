7f16fcfd710d6257f03e59fb6ee3f762773c552a
kyunghoon <rlarudgns970@naver.com>
2023-01-29 11:06:32 +0900

    .

--- news/main.js
+++ news/main.js
@@ -1,6 +1,9 @@
-let news = []
+let news = [];
 let menus = document.querySelectorAll(".menus button");
-menus.forEach((menu) => menu.addEventListener("click", (event)=> getNewsByTopic(event)))
+menus.forEach((menu) => menu.addEventListener("click", (event) => getNewsByTopic(event)))
+
+let searchButton = document.getElementById("search-button");
+
 const getLatestNews = async() =>{
     let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10`);
     let header = new Headers({'x-api-key' : 'bmIy8Z4kdrrGIXjYjjfTCRjDnYdhPkXvHTPcDtO-JCI'});
@@ -10,7 +13,7 @@ const getLatestNews = async() =>{
     news = data.articles;
     console.log(news);
 
-    render()
+    render();
 };
 
 const getNewsByTopic = async (event) =>{
@@ -27,9 +30,31 @@ const getNewsByTopic = async (event) =>{
     render();
 };
 
+const getNewsByKeyword = async () => {
+    // 1. 검색 키워드 읽어오기
+    // 2. url에 검색 키워드 부치기
+    // 3. 헤더 준비
+    // 4. url 부르기
+    // 5. 데이터 가져오기
+    // 6. 데이터 보여주기
+
+    let keyword = document.getElementById("search-input").value;
+    let url = new URL(
+        `https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`
+        );
+    let header = new Headers({
+        'x-api-key' : 'bmIy8Z4kdrrGIXjYjjfTCRjDnYdhPkXvHTPcDtO-JCI'
+    });
+    let response = await fetch(url,{headers:header});
+    let data = await response.json();
+    news = data.articles
+    render();
+};
+
 const render = () => {
-    let newsHTML = ''
-    newsHTML = news.map((item) => {
+    let newsHTML = '';
+    newsHTML = news
+      .map((item) => {
         return `<div class="row news">
         <div class="col-lg-4">
             <img class="news-img-size" src="${item.media}"/>
@@ -47,8 +72,8 @@ const render = () => {
     }).join('');
 
 
-    document.getElementById("news-board").innerHTML = newsHTML
+    document.getElementById("news-board").innerHTML = newsHTML;
+};
 
-}
-
+searchButton.addEventListener("click", getNewsByKeyword);
 getLatestNews()
